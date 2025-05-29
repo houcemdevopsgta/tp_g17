185e2662c7d41847c613d09d4b798e73a6134142
houcemdevopsgta <134325311+houcemdevopsgta@users.noreply.github.com>
2025-05-29 11:29:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps!!!');
+  res.send('Hello Efi-Academy!!!');
 });
 
 const PORT = process.env.PORT || 3000;
