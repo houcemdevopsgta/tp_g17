1515683353df02261865aa8e6f5c66cefcf02b8c
houcemdevopsgta <134325311+houcemdevopsgta@users.noreply.github.com>
2025-09-03 16:20:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Devops from MyProject!!!');
+  res.send('Hello Devops from MyProject V2!!!');
 });
 
 const PORT = process.env.PORT || 3000;
